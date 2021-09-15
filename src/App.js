e53ca49a51fb3b020eb810398b41492c96ec938e
locDEV169 <48356607+locDEV169@users.noreply.github.com>
2021-09-15 13:35:13 +0700

    call Api with useEffect + fetch + await + async

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/Todo';
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './components/TodoFrom';
+import PostList from './components/PostList';
 
 function App() {
-  const [todoList,setTodoList] = useState([
-    {id:1,title: "I ... 1"},
-    {id:2,title: "I ... 2"},
-    {id:3,title: "I ... 3"},
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I ... 1" },
+    { id: 2, title: "I ... 2" },
+    { id: 3, title: "I ... 3" },
   ]);
+  //tạo danh sách post lấy từ Api về
+  const [postList, setPostList] = useState([]);
+  //sử dụng hook useEffect()
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        // link api
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        // gọi file có trong api
+        const response = await fetch(requestUrl);
+        // chuyển file response sang file json
+        const responseJSON = await response.json();
+        console.log({ responseJSON })
+        // llấy dữ liệu data từ responseJSON
+        const { data } = responseJSON;
+        //console.log({data})
+        setPostList(data)
+      } catch (error) {
+        console.log("failed to fetch post List" , error.message)
+      }
+    }
 
-  function handleTodoClick(todo){
+    // gọi để chạy function phía trên
+    fetchPostList();
+  }, []);
+
+  function handleTodoClick(todo) {
     console.log(todo)
-    const index = todoList.findIndex(x => x.id == todo.id)
-    if(index < 0 ) return;
+    const index = todoList.findIndex(x => x.id === todo.id)
+    if (index < 0) return;
     const newTodoList = [...todoList];
-    newTodoList.splice(index,1);
+    newTodoList.splice(index, 1);
     setTodoList(newTodoList)
   }
 
-  function handleTodoSubmit(formValues){
+  function handleTodoSubmit(formValues) {
     console.log("Form Values", formValues)
     //add new todo to current todoList
     const newTodo = {
@@ -36,10 +62,12 @@ function App() {
     <div className="App">
       <h1>Welcome to React Hook</h1>
       <ColorBox />
-      <h2>React Hook lab2 -TodoList</h2>
-      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
-      <h2>React Hook lab3 -TodoForm</h2>
-      <TodoForm onSubmit={handleTodoSubmit}/>
+      <h2>React Hook useState lab2 -TodoList</h2>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h2>React Hook useState lab3 -TodoForm</h2>
+      <TodoForm onSubmit={handleTodoSubmit} />
+      <h2>React Hook useEffect lab1 - PostList Api</h2>
+      <PostList posts={postList} />
     </div>
   );
 }
